6228c22578853657e9ad7589300407b52f660958
tusharbhavnani@gmail.com <tushar1221>
2015-11-05 14:05:57 +0800

    starting of my game

--- game/game.js
+++ game/game.js
@@ -8,6 +8,17 @@ $(document).ready(function() {
   // ask for your name
 var name = prompt("What's your name");
 //The question you find you will go ahead but if you anwser it wrong you would die
+var Life=1
+function LifeCheck() {
+// this function checks the lifeline and if they have 0 it will brain them to the end screen!
+  if (Life===0) {
+   alert("YOU HAVE LOST THE GAME!");
+   alert("Great Job making so far in this question. Your score will be recorded! Better Luck Next Time!");
+   throw new Error("GAME OVER");
+  }
+   
+}
+
 
 
 });
