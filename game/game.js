acbac5050fe7aa7aa4bdbeba99c06a5929d8c3a5
tusharbhavnani@gmail.com <tushar1221>
2015-11-05 10:28:09 +0800

    New Play Remove Button

--- game/game.js
+++ game/game.js
@@ -1,5 +1,8 @@
 //this is my JS file
 <button onClick="document.getElementById('d3').style.display='block'"></button>
+$(#Start).(click(function() {
+	$(#bodyID).css("background", "url('images/bg.jpg')");
+)};
 // ask for your name
 var name = prompt("What is your name?"):
 // 
